fb7b39ebbbcd5df1eb9570f498e0f2ca70c232d3
Jeremy Monat <jemonat@calalum.org>
2022-07-13 19:15:17 -0400

    Added x as second parameter for each solve()

--- system-of-inequalities/limited-to-one-symbol.py
+++ system-of-inequalities/limited-to-one-symbol.py
@@ -3,5 +3,7 @@ from sympy import solve, symbols
 x, y = symbols("x y")
 from sympy.abc import x, y
 
-solve([x ** 2 < y, x > 0])
-
+solve([x ** 2 < y, x > 0], x)
+# Traceback (most recent call last):
+#     ...
+# NotImplementedError: inequality has more than one symbol of interest.
--- system-of-inequalities/title-example.py
+++
@@ -1,6 +0,0 @@
-from sympy import symbols, solve
-from sympy.core.relational import Relational
-
-x = symbols("x")
-eq = solve([x >= 0.5, x <= 0.7])
-[(i.rhs, i.rel_op, i.lhs) for i in [i.canonical for i in eq.atoms(Relational)]]
